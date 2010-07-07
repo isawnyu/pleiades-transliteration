45d68c8c1169cd435182bf0783d386e1f2123f26
thomase <thomase@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2010-07-07 16:11:49 +0000

    adding GPL indicia

--- pleiades/transliteration/ws_latn.py
+++ pleiades/transliteration/ws_latn.py
@@ -1,3 +1,19 @@
+# Copyright 2009, 2010 Institute for the Study of the Ancient World, New York University
+
+# This file is part of pleiades.transliteration
+
+# pleiades.transliteration is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pleiades.transliteration is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pleiades.transliteration.  If not, see <http://www.gnu.org/licenses/>.
 
 latn_capital = {
     u"A" : "A",    # Latin capital letter a
