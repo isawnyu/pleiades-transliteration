0704112c871f6e52a3047f52bc121fdbf722b8c6
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-01-11 18:38:29 +0000

    Add "en" for English

--- pleiades/transliteration/__init__.py
+++ pleiades/transliteration/__init__.py
@@ -8,11 +8,12 @@ def transliterate_name(lang, name_utf8):
         name = name_utf8[:]
     else:
         name = unicode(name_utf8, 'utf-8')
-    if wsystem == 'grc' or wsystem == 'la-grek':
-        transliteration = ws_grek.transliterate(name)
+    if wsystem == 'en':
+        return name
+    elif wsystem == 'grc' or wsystem == 'la-grek':
+        return ws_grek.transliterate(name)
     elif wsystem == 'la' or wsystem == 'grc-latn':
-        transliteration = ws_latn.transliterate(name)
+        return ws_latn.transliterate(name)
     else:
         raise ValueError, 'Unsupported writing system (%s)' % lang
-    return transliteration
     
